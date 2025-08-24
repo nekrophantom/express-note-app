2c8aa3bba2f4ea037b9bccc65779ee531fecd5f1
Reynold <reynoldandikagradiyanto@gmail.com>
2025-08-24 19:42:45 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import express, { urlencoded } from 'express'
 import dotenv from 'dotenv'
 import indexRoute from './routes/index.route.js'
-
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
